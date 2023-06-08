ddac06c034b256813e5267ab0cc10d0f5437b561
Codemartino <akudugumartin5@gmail.com.com>
2023-06-08 12:06:05 -0700

    infinit addition of numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
+from sys import argv
+
+
 if __name__ == "__main__":
-    import sys
-    leng = len(sys.argv)
-    sum = 0
-    for k in range(leng):
-        if k == 0:
-            continue
-        sum += int(sys.arg [k])
-    print('{}'.format(sum))
+
+    print(sum(map(int, argv[1:])))
