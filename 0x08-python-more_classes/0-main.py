9cd4f906103dbd9dec6a1d9ee2a3e53062ba422e
Codemartino <akudugumartin5@gmail.com.com>
2023-07-03 12:15:49 -0700

    updated

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 Rectangle = __import__('0-rectangle').Rectangle
-### this code write empty class rectangle ### 
+"""this code write empty class rectangle""" 
 
 my_rectangle = Rectangle()
 print(type(my_rectangle))
