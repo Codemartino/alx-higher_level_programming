11d79ec83a848d6ac59173f4f76e6f21b21ec2e1
Codemartino <akudugumartin5@gmail.com.com>
2023-07-05 02:24:28 -0700

    updated

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Declare a matrix multiplication function."""
-
+"""Defines a matrix multiplication function."""
 
 
 def matrix_mul(m_a, m_b):
@@ -40,7 +39,7 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+        raise TypeError("each row of m_a must should be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must should be of the same size")
 
