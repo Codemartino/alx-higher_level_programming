c65359a378a14a7f1e2674a6e52795f2eaef4dcc
Codemartino <akudugumartin5@gmail.com.com>
2023-07-10 10:15:16 -0700

    updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" Manipulate lists of integers
-"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """ Manage a list of integers
-    """
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """ Print a list sorted in ascending order
-        """
+        """Print a list in sorted ascending order."""
         print(sorted(self))
