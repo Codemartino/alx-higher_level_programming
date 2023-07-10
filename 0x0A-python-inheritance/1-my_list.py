791149f22bb2c80a690fc301e0bd1c74936c1ebb
Codemartino <akudugumartin5@gmail.com.com>
2023-07-10 09:58:01 -0700

    updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Manipulate lists of ints
+""" Manipulate lists of integers
 """
 
 
 class MyList(list):
-    """ Manage a list of ints
+    """ Manage a list of integers
     """
     def print_sorted(self):
         """ Print a list sorted in ascending order
