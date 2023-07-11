e5b6c80f5ce9071650f046ec131b6f8902105dbe
Codemartino <akudugumartin5@gmail.com.com>
2023-07-11 13:17:33 -0700

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Provides a function to count the lines in a file"""
+"""Defines a file-writing function."""
 
 
-def number_of_lines(filename=""):
-    """Count the number of lines in a file"""
-    with open(filename, 'r') as istream:
-        return len(istream.readlines())
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
