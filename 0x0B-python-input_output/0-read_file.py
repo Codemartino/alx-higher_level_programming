abf412d921ab333055f5942040a980c31bb5e6ce
Codemartino <akudugumartin5@gmail.com.com>
2023-07-11 13:00:47 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Provides a function to read and print a file"""
+
+
 def read_file(filename=""):
-    """ Function that read a text file (UTF8) and prints it stdout """
-    with open(filename, encoding='utf-8') as f:
-        read_file = f.read()
-    print('{:s}'.format(read_file), end="")
+    """Print the contents of a file"""
+    with open(filename, "r") as istream:
+        print(istream.read(), end="")
