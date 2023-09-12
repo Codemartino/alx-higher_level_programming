f22fb9e321ba34e2d3a7de9c649d62d0fbf54dc4
Codemartino <akudugumartin5@gmail.com>
2023-09-12 00:20:58 +0000

    fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-t MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+#!/usr/bin/node
+const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(MyArray[i]);
 }
