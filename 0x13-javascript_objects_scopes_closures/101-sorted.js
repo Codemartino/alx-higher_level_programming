382c077b3182622103511b9e8c0345e001a76490
Codemartino <akudugumartin5@gmail.com>
2023-09-13 00:43:35 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+// Compute a dictionary of user IDs by occurrence
+const occurrencesByUser = require('./101-data').dict;
+const usersByOccurrence = {};
+let user;
+let occurrence;
+for (user in occurrencesByUser) {
+  occurrence = occurrencesByUser[user];
+  if (usersByOccurrence[occurrence] === undefined) {
+    usersByOccurrence[occurrence] = [user];
+  } else {
+    usersByOccurrence[occurrence].push(user);
+  }
+}
+console.log(usersByOccurrence);
