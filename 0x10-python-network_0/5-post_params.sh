a4e681c12d98658a420853747257feb017478b35
Codemartino <akudugumartin5@gmail.com>
2023-09-28 10:56:42 -0700

    5-post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a POST request to a URL and display the body of the response
-curl -sL -d 'test@gmail.com' -d 'subject=I will always be here for PLD' -- "$1"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
