b39b4caaccb0f723d103ac78d32ceb5597471e39
Codemartino <akudugumartin5@gmail.com>
2023-09-28 10:53:23 -0700

    4-header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a URL and display the body of the response
-curl -sL -H 'X-School-User-Id: 98' -- "$1"i
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-School-User-Id: 98" "${1}"
