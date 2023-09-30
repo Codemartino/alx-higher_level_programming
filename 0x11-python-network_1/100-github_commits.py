2afd2121d5a26f720196bb9645f935b3455a2e24
Codemartino <akudugumartin5@gmail.com>
2023-09-30 01:27:37 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,27 @@
-st 10 commits (newest to oldest) of the repo "rails" by the user "rails"
-'''
+#!/usr/bin/python3
 
-import sys
-import requests
+"""
+holberton interview :
+Write a Python script that takes 2 arguments in order to solve this challenge.
 
-URLFMT = 'https://api.github.com/repos/{owner}/{repo}/commits'
+    The first argument will be the repository name
+    The second argument will be the owner name
+    You must use the packages requests and sys
+    You are not allowed to import packages other than requests and sys
+    You don’t need to check arguments passed to the script (number or type)
+
+"""
 
 if __name__ == '__main__':
-    response = requests.get(URLFMT.format(repo=sys.argv[1], owner=sys.argv[2]))
-    for item in response.json()[:10]:
-        print('{}: {}'.format(item['sha'], item['commit']['author']['name']))
+    import sys
+    import requests
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    repo_info = owner_name + "/" + repo_name
+    url = "https://api.github.com/repos/" + repo_info + "/commits"
+    r = requests.get(url)
+    top = r.json()[:10]
+    for i in top:
+        el = i['sha']
+        author = i['commit']['author']['name']
+        print('{}: {}'.format(el, author))
