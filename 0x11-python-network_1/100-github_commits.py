c267855fee99599423f32ea25638fc688ba377a1
Codemartino <akudugumartin5@gmail.com>
2023-09-30 01:24:18 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+st 10 commits (newest to oldest) of the repo "rails" by the user "rails"
+'''
+
+import sys
+import requests
+
+URLFMT = 'https://api.github.com/repos/{owner}/{repo}/commits'
+
+if __name__ == '__main__':
+    response = requests.get(URLFMT.format(repo=sys.argv[1], owner=sys.argv[2]))
+    for item in response.json()[:10]:
+        print('{}: {}'.format(item['sha'], item['commit']['author']['name']))
